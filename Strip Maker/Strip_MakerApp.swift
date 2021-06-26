3f43a868319953c08389504dfa78f7bc00c64393
Jeremy <jeremybahadirli@gmail.com>
2021-06-26 13:58:51 -0500

    Initial Commit

---
+++ Strip Maker/Strip_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Strip_MakerApp.swift
+//  Strip Maker
+//
+//  Created by Jeremy Bahadirli on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Strip_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
